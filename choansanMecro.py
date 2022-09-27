3cbe375a4546f55ae0a255e19fa32998bd2bd5e8
souk0721 <souk0721.gmail.com>
2022-09-27 10:59:40 +0900

    20220928

--- choansanMecro.py
+++ choansanMecro.py
@@ -11,7 +11,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option("useAutomationExtension", False)
-options.add_experimental_option("detach", True) # 화면이 꺼지지 않고 유지
+options.add_experimental_option("detach", False) # 화면이 꺼지지 않고 유지
 options.add_experimental_option("excludeSwitches", ["enable-logging"]) # 불필요한 메세지 제거
 options.add_experimental_option("excludeSwitches", ["enable-automation"]) # 자동화 메세지 제거
 service = ChromeService(executable_path="chromedriver.exe")
@@ -19,36 +19,30 @@ browser = webdriver.Chrome(service=service, options=options)
 browser.get('https://reservation.nowonsc.kr/member/login')
 today_day = datetime.now().day
 
+def site_login():
+    ## ID 위치
+    id_box = browser.find_element(By.XPATH, '//*[@id="memberId"]').send_keys(config.id)
+    time.sleep(1)
+    pw_box = browser.find_element(By.XPATH, '//*[@id="memberPassword"]').send_keys(config.pw)
+    # WebDriverWait(browser, timeout=10)
+    time.sleep(1)
+    ## 로그인 클릭
+    login_click=browser.find_element(By.XPATH, '//*[@id="frm"]/fieldset/div/div[4]/button')
+    login_click.click()
+    time.sleep(3)
+    ## 비밀번호 변경 문구
+    pw_change = browser.find_element(By.XPATH, '//*[@id="frm"]/fieldset/div[2]/div/a')
+    print(pw_change)
+    # if pw_change:
+    #     print('문구발생')
+    #     pw_change.click()
+    #     time.sleep(1)
+    choansan = browser.get('https://reservation.nowonsc.kr/leisure/camping_date?cate1=2')
+    time.sleep(3)
 
-
-assert '노원구' in browser.title
-
-## ID 위치
-id_box = browser.find_element(By.XPATH, '//*[@id="memberId"]').send_keys(config.id)
-time.sleep(1)
-pw_box = browser.find_element(By.XPATH, '//*[@id="memberPassword"]').send_keys(config.pw)
-# WebDriverWait(browser, timeout=10)
-time.sleep(1)
-## 로그인 클릭
-login_click=browser.find_element(By.XPATH, '//*[@id="frm"]/fieldset/div/div[4]/button')
-login_click.click()
-time.sleep(3)
-## 비밀번호 변경 문구
-pw_change = browser.find_element(By.XPATH, '//*[@id="frm"]/fieldset/div[2]/div/a')
-print(pw_change)
-# if pw_change:
-#     print('문구발생')
-#     pw_change.click()
-#     time.sleep(1)
-
-
-
-## 초안산 예약 페이지 입장
-choansan = browser.get('https://reservation.nowonsc.kr/leisure/camping_date?cate1=2')
-time.sleep(3)
 
 def reservation(month):
-## 예약 가능할 날짜를 반환한다.
+    ## 예약 가능할 날짜를 반환한다.
     before_month = browser.find_element(By.XPATH,'//*[@id="frm"]/div[1]/div[1]/div[1]/div/div/div[1]')
     display_month = browser.find_element(By.XPATH,'//*[@id="frm"]/div[1]/div[1]/div[1]/div/div/div[2]')
     display_month = display_month.get_attribute('data-nowmonth')
@@ -86,33 +80,14 @@ def reservation(month):
                     # j.click()
                     # time.sleep(1)
                     # reserve_button = browser.find_element(By.XPATH, '//*[@id="reserved_submit"]').click() 
-                
+
 if __name__ == '__main__':
+    site_login()
+    time.sleep(1)
+    
     while True:
+       
         reservation(datetime.now().month)
         time.sleep(1)
         reservation(datetime.now().month+1)
         time.sleep(1)
-
-
-
-# print(table)
-## 다음달 XPATH : //*[@id="frm"]/div[1]/div[1]/div[1]/div/div/div[3]
-## 39 - 65 
-## 파크캠핑 빌리지 구역 방번호 == /html/body/div[3]/div[1]/div[2]/form/div[1]/div[2]/div[2]/div/ul/li[1]/input
-## 파크캠핑 빌리지 구역 방번호 == //*[@name="village2"]
-## 파크캠핑 빌리지 구역 라디오 버튼 Xpath == //*[@id="village01"]
-
-
-## 금일을 기준으로 26일 이후에는 다음달을 검색한다.
-# if int(today_day)>25:
-#     right_click = browser.find_element(By.XPATH,'//*[@id="frm"]/div[1]/div[1]/div[1]/div/div/div[3]')
-#     right_click.click()
-#     time.sleep(10)
-    
-
-    
-
-
-# browser.find_element(by=By.NAME,value='//*[@id="memberId"]').send_keys('souk0721')
-# browser.find_element('//*[@id="memberPassword"]').send_keys('shgustjr1!')
